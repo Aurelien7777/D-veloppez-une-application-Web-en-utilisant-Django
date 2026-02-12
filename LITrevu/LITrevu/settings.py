e539be6ceed7a6e873e6e55cdba9717ab6d1526c
Aurélien Amorin <amorin77184@gmail.com>
2026-02-12 09:31:21 +0100

    Déplacement du dossier templates dans le bon dossier LITrevu et modification de settings templates

--- LITrevu/LITrevu/settings.py
+++ LITrevu/LITrevu/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'LITrevu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['LITrevu/users/templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
